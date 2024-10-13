90b0647888005e99cb02782400bba6173520cedf
Nikolai192 <nikki_krasava@mail.ru>
2024-10-13 21:16:02 +0300

    hw11 готовая

---
+++ src/Author.java
@@ -0,0 +1,15 @@
+public class Author {
+    private String nameAuthor;
+    private String familyAuthor;
+
+    public Author(String nameAuthor, String familyAuthor) {
+        this.nameAuthor = nameAuthor;
+        this.familyAuthor = familyAuthor;
+    }
+    public String getNameAuthor() {
+        return  nameAuthor;
+    }
+    public String getFamilyAuthor() {
+        return familyAuthor;
+    }
+}
